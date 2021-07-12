651dc547706d6d28f93985c9613c5a75655b8fd5
Starle21 <staronova.lenka@gmail.com>
2021-07-12 15:57:06 +0200

    Part 4.5* Unit tests step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -8,7 +8,22 @@ const totalLikes = (blogs) => {
   }, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  return blogs.reduce(
+    (max, blog) => {
+      if (blog.likes > max.likes) {
+        max.title = blog.title;
+        max.author = blog.author;
+        max.likes = blog.likes;
+      }
+      return max;
+    },
+    { title: "", author: "", likes: 0 }
+  );
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };
