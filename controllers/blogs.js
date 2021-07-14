07b9ab84a209454b47257a12004bff861445c2b8
Starle21 <staronova.lenka@gmail.com>
2021-07-14 09:22:25 +0200

    Part 4.8 Bloglist API tests, step1

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,17 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+// // CHAINING PROMISES
+// blogsRouter.get("/", (request, response) => {
+//   Blog.find({}).then((blogs) => {
+//     response.json(blogs);
+//   });
+// });
+
+// ASYNC AWAIT
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
 blogsRouter.post("/", (request, response) => {
